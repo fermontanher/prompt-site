876ea218235aebeff6d2ea9a2a249c15ed62f53b
fermontanher <fer.montanher@gmail.com>
2026-04-09 13:52:52 -0300

    Integrate Google Tag Manager in _app.js
    Added Google Tag Manager scripts for tracking.

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,22 @@
 import "../styles/globals.css";
+import Script from "next/script";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=AW-18076770776"
+        strategy="afterInteractive"
+      />
+      <Script id="google-ads" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-18076770776');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
